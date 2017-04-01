e34cf0d89e33c7fd3335b7d8ad3bfb4a2c4c3d5f
lsr488 <lsr488@gmail.com>
2017-04-01 17:41:37 -0400

    Added title and header to page.

--- spec/views/prototype_spec.rb
+++ spec/views/prototype_spec.rb
@@ -2,5 +2,7 @@ RSpec.feature 'Prototype' do
   scenario 'user visits the prototype page' do
     visit '/prototype'
     expect(page).to have_css('h1', text: 'Hello, world!')
+    expect(page).to have_css('header h1', text: 'Phrasebook')
+    expect(page).to have_title "Phrasebook"
   end
 end
