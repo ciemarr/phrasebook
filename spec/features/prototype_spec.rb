22bec876b248c2f8b99ab3ab98167e41edb20f27
lsr488 <lsr488@gmail.com>
2017-04-09 20:39:39 -0400

    Subdivided lists.

--- spec/features/prototype_spec.rb
+++ spec/features/prototype_spec.rb
@@ -1,13 +1,18 @@
 RSpec.feature 'Prototype' do
-  scenario 'user visits the prototype page' do
+  before(:each) do
     visit '/prototype'
-    expect(page).to have_css('h1', text: 'Hello, world!')
+  end
+
+  scenario 'user visits the prototype page' do
     expect(page).to have_css('header h1', text: 'Phrasebook')
     expect(page).to have_title 'Phrasebook'
   end
 
   scenario 'user sees a static list of words' do
-    visit '/prototype'
     expect(page).to have_css('ul li', count: 4)
+  end
+
+  scenario 'user sees subdivided list of words' do
+    expect(page).to have_css('h2', count: 2)
   end
 end
